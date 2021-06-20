34cba5ff76f60e6d00ed23858f860a8574c1d065
Vlad Rassokhyn <perishvlad@gmail.com>
2021-06-20 21:40:38 +0300

    feat: add selector-opening animation

--- src/common/Selector.tsx
+++ src/common/Selector.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from "react";
 import { useAppDispatch } from "../lib/store/hooks";
 import { stProductPerPage } from "../lib/store/ProductReducer";
 
@@ -7,34 +7,35 @@ type P = {
   arr: number[]
 }
 
-export const Selector: React.FC<P> = ({current, arr}) => {
+export const Selector: React.FC<P> = ({ current, arr }) => {
 
   const dispatch = useAppDispatch();
   const [visible, setVisible] = React.useState(false);
 
   const handleClick = (option: number) => {
     setVisible(!visible);
-    dispatch(stProductPerPage(option))
+    dispatch(stProductPerPage(option));
     // eslint-disable-next-line no-undef
-    window.scrollTo({top: 0, behavior: "smooth"})
-  }
+    window.scrollTo({ top: 0, behavior: "smooth" });
+  };
 
   const options = arr.map((option: number, i: number) => {
     if (option !== current)
-    return <span
-      key={i}
-      className={'selector-label'}
-      style={{top: `-${(i+1)*40} px`}}
-      onClick={() => handleClick(option)}
-    >
+      return <span
+        key={i}
+        className={"selector-label"}
+        onClick={() => handleClick(option)}
+      >
       {option}
-    </span>
-  })
+    </span>;
+  });
 
-  return <div className={'selector-wrapper'}>
-    <div className={'selector-label selector-label-current'} onClick={() => setVisible(!visible)}>
+  return <div className={"selector-wrapper"}>
+    <div className={"selector-label selector-label-current"} onClick={() => setVisible(!visible)}>
       <h1>{current}</h1>
     </div>
-    {visible && options}
-  </div>
-}
+    <div className={`selector-options-list ${visible && 'open-selector'}`}>
+      {visible && options}
+    </div>
+  </div>;
+};
