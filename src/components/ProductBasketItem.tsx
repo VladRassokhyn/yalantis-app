1bbc637ae4ee900334fe766d9fb551ef0000fc2b
Vlad Rassokhyn <perishvlad@gmail.com>
2021-06-20 17:26:34 +0300

    feat: add Link to product in basket

--- src/components/ProductBasketItem.tsx
+++ src/components/ProductBasketItem.tsx
@@ -1,13 +1,14 @@
 import React from "react";
 import { IBasket } from "../lib/types";
 import defaultProductPhoto from '../static/defaultProductPhoto.svg';
+import { Link } from "react-router-dom";
 
 export const ProductBasketItem: React.FC<{ item: IBasket }> = ({item}) => {
 
   return <div className={'basket-item-wrapper'}>
     <img src={item.photo ? item.photo : defaultProductPhoto} alt={'image'}/>
     <div className={'basket-item-title-container'}>
-      <h1>{item.name}</h1>
+      <Link to={`products/${item.id}`}><h1>{item.name}</h1></Link>
       <h2><b>Price:</b> {item.price}$</h2>
       <h2><b>Count:</b> {item.count}</h2>
     </div>
