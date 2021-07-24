b0da10be1a3a535f18a8d30138242e65c97ea8ed
Vlad Rassokhyn <perishvlad@gmail.com>
2021-07-24 21:39:49 +0300

    fix: remove redirect, it clear query params

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,19 +1,18 @@
 import React from 'react';
-import { Redirect, Switch } from 'react-router-dom';
+import { Switch} from 'react-router-dom';
 import { Routes, routes } from '../lib/router/routes';
 import { Header } from '../components/Header';
 import { Notificator } from '../components/Notificator';
-import { ROUTE_PATHS } from '../lib/router/paths';
 
-export const Main = () => (
-  <div>
-    <Header />
-    <Notificator />
+export const Main = () => {
+  return <div>
+    <Header/>
+    <Notificator/>
     <Switch>
       {routes.map((route, i) => (
         <Routes key={i} {...route} />
       ))}
     </Switch>
-    <Redirect to={ROUTE_PATHS.PRODUCTS.BASE()}/>
-  </div>
-);
+
+  </div>;
+};
